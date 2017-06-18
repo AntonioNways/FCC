60d9beec1217708a1033075651983677e858e277
Silver Dn <Antonion90@gmail.com>
2017-06-17 23:22:27 -0400

    Add initial state of App

--- Markdown/app/app.js
+++ Markdown/app/app.js
@@ -2,6 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return { 
+      "text": "Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain."
+    };
+  },
   render : function() {
     return (
       <div>
@@ -9,6 +14,7 @@ var App = React.createClass({
         <div id="header"></div>
         <div className="container">
           <div className="column">
+            <textarea>{this.state.text}</textarea>
           </div>
           <div className="column"></div>
           <div className="column"></div>
