12e8843e8f3266e15a2f819e360466c3315ba7b6
Silver Dn <Antonion90@gmail.com>
2017-06-21 01:14:32 -0400

    Add sorting highlight

--- Camper-Leaderboard/app/app.js
+++ Camper-Leaderboard/app/app.js
@@ -2,6 +2,20 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 import $ from 'jquery';
 
+var recentPoint ="Points in past 30*";
+var allPoint = "All time points";
+
+function sortLeaderBy(type){
+  if(type){
+    recentPoint ="Points in past 30*";
+    allPoint = "All time points";
+  }
+  else{
+    recentPoint ="Points in past 30"; 
+    allPoint = "All time points*";
+  }
+}
+
 var App = React.createClass({
   getInitialState: function() {
     return { 
@@ -33,10 +47,10 @@ var App = React.createClass({
             <table width="90%">
               <thead>
                 <tr>
-                  <th width="10%">Rank</th>
-                  <th width="40%">Camper Name</th>
-                  <th width="25%"><a onClick={function(){this.getdata("recent")}.bind(this)}>Points in past 30</a></th>
-                  <th width="25%"><a onClick={function(){this.getdata("all")}.bind(this)}>All time points </a></th>
+                  <th width="15%">Rank</th>
+                  <th width="35%">Camper Name</th>
+                  <th width="25%"><a onClick={function(){sortLeaderBy(true);return this.getdata("recent")}.bind(this)}>{recentPoint}</a></th>
+                  <th width="25%"><a onClick={function(){sortLeaderBy(false);return this.getdata("all")}.bind(this)}>{allPoint}</a></th>
                 </tr>
               </thead>
                 <TablePane data={this.state.data}/>
@@ -52,10 +66,10 @@ var TablePane = React.createClass({
   renderTableInfoPane: function(rank){
     return (
           <tr key={rank}>
-            <td>{Number(rank)+1}</td>
+            <td><center>{Number(rank)+1}</center></td>
             <td><img src={this.props.data[rank].img} height="42" width="42"/>{this.props.data[rank].username}</td>
-            <td>{this.props.data[rank].recent}</td>
-            <td>{this.props.data[rank].alltime}</td>
+            <td><center>{this.props.data[rank].recent}</center></td>
+            <td><center>{this.props.data[rank].alltime}</center></td>
           </tr>
     );
   },
