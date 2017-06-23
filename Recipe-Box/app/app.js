80e20feb6715bb54c4c69e6d70c65e3cd9e7613f
Silver Dn <Antonion90@gmail.com>
2017-06-23 02:01:32 -0400

    Add function to recipe button

--- Recipe-Box/app/app.js
+++ Recipe-Box/app/app.js
@@ -1,16 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var initialRecipe = {"recipe":{"Bread Pudding":{"ingredients":"2 cups milk, 1/4 cup butter or margarine, 2 eggs, 1/2 cup sugar, cinnamon, 1/4 teaspoon salt. 6 slices of bread","instructions": "1. Heat oven to 350ºF. In 2-quart saucepan, heat milk and butter over medium heat until butter is melted and milk is hot. 2.Mix eggs, sugar, cinnamon and salt. Stir in bread cubes. Stir in milk mixture. Pour into ungreased deep round pan., 3.Bake uncovered for 40 to 45 minutes " },"Bread Pudding2":{"ingredients":"2 cups milk, 1/4 cup butter or margarine, 2 eggs, 1/2 cup sugar, cinnamon, 1/4 teaspoon salt. 6 slices of bread","instructions": "1. Heat oven to 350ºF. In 2-quart saucepan, heat milk and butter over medium heat until butter is melted and milk is hot. 2.Mix eggs, sugar, cinnamon and salt. Stir in bread cubes. Stir in milk mixture. Pour into ungreased deep round pan., 3.Bake uncovered for 40 to 45 minutes " }}}
+var initialRecipe = {"recipe":{"Bread Pudding":{"ingredients":"2 cups milk, 1/4 cup butter or margarine, 2 eggs, 1/2 cup sugar, cinnamon, 1/4 teaspoon salt. 6 slices of bread","instructions": "1. Heat oven to 350ºF. In 2-quart saucepan, heat milk and butter over medium heat until butter is melted and milk is hot. 2.Mix eggs, sugar, cinnamon and salt. Stir in bread cubes. Stir in milk mixture. Pour into ungreased deep round pan., 3.Bake uncovered for 40 to 45 minutes " },"Bread Pudding2":{"ingredients":"2 cups milk, 1/4 cup butter or margarine, 2 eggs, 1/2 cup sugar, cinnamon, 1/4 teaspoon salt. 6 slices of bread","instructions": "1. Heat oven to 350ºF. In 2-quart saucepan, heat milk and butter over medium heat until butter is melted and milk is hot. 2.Mix eggs, sugar, cinnamon and salt. Stir in bread cubes. Stir in milk mixture. Pour into ungreased deep round pan., 3.Bake uncovered for 40 to 45 minutes " }}};
+var temp="";
 
 var App = React.createClass({
   getInitialState: function() {
     return { 
-      "recipe":{}   
+      "recipe":{},
+      "selectRecipe":{}  
     };
   },
   componentWillMount: function(){
     this.setState(initialRecipe);
+  },
+  setSelectRecipe: function(recipe_index){
+    this.setState({
+      selectRecipe: {"ingredients":this.state.recipe[recipe_index].ingredients,
+    "instructions":this.state.recipe[recipe_index].instructions}
+    });
   },
   render : function() {
     return (
@@ -25,7 +33,7 @@ var App = React.createClass({
                   <th width="15%"><h2>Recipe</h2></th>
                 </tr>
               </thead> 
-                <RecipeList recipe={this.state.recipe}/>
+                <RecipeList recipe={this.state.recipe} setSelectRecipe={this.setSelectRecipe}/>
             </table>
             
             
@@ -39,21 +47,34 @@ var App = React.createClass({
 });
 
 var RecipeList = React.createClass({
-  renderTableInfoPane: function(recipe){
+  renderTableItem: function(name){//<stuck here, need to find how to get the key correctly
+    return <RecipeItem key={name} name={name} itemRecipe={this.props.recipe[name]}/>
+  },
+  render: function() {
     return (
-          <tr key={recipe}>
-            <td><h4>{recipe}</h4>
-              <button type="button" className="btn btn-info">Recipe</button>{" "}
+      <tbody>{Object.keys(this.props.recipe).map(this.renderTableItem)}</tbody>
+    )
+  }
+});
+
+var RecipeItem = React.createClass({
+  setSelected: function(){
+    this.setState({selectRecipe: this.props.itemRecipe});
+    console.log(this.state)
+  },
+  render: function(){//<stuck here, need to find how to get the key correctly
+    return (
+          <tr key={this.props.name}>
+            <td><h4>{this.props.name}</h4>
+              <button type="button" className="btn btn-info" onClick={this.setSelected}>Recipe</button>{" "}
               <button type="button" className="btn btn-warning">Edit</button>{" "}
               <button type="button" className="btn btn-danger">Delete</button></td>
           </tr>
     );
   },
-  render: function() {
-    return (
-      <tbody>{Object.keys(this.props.recipe).map(this.renderTableInfoPane)}</tbody>
-    )
-  }
+
 });
+
+
 
 ReactDOM.render (<App />, document.getElementById("container"));
