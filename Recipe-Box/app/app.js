7cd265ee1cbaced85ad58d3baa1543ce49849505
Silver Dn <Antonion90@gmail.com>
2017-06-29 00:08:44 -0400

    Add edit recipe function

--- Recipe-Box/app/app.js
+++ Recipe-Box/app/app.js
@@ -2,26 +2,82 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var initialRecipe = {"recipe":{"Bread Pudding":{"Ingredients":"2 cups milk, 1/4 cup butter or margarine, 2 eggs, 1/2 cup sugar, cinnamon, 1/4 teaspoon salt, 6 slices of bread.","Instructions": "1. Heat oven to 350ºF. In 2-quart saucepan, heat milk and butter over medium heat until butter is melted and milk is hot. 2.Mix eggs, sugar, cinnamon and salt. Stir in bread cubes. Stir in milk mixture. Pour into ungreased deep round pan. 3.Bake uncovered for 40 to 45 minutes " },"Mac and cheese":{"Ingredients":"4 cups/1L full cream milk, 2 cups macaroni, 2 cups cheddar cheese","Instructions": "1.Place milk into a small saucepan and bring to the boil over a medium heat. 2.Add the pasta and return to the boil, reduce heat to simmer for 8 minutes. 3. Stir through cheese and leave to sit for 2 minutes." }}};
-var temp="";
+
 
 var App = React.createClass({
   getInitialState: function() {
     return { 
       "recipe":{},
-      "selectRecipe":{}  
+      "selectRecipe":{},
+      "editRecipe":{},
+      "type": "",
     };
   },
   componentWillMount: function(){
     this.setState(initialRecipe);
   },
-  setSelectRecipe: function(recipe_index){
+  setSelectRecipe: function(recipe_index,type){
+    this.setState({type :type});
+    if(type==="Delete"){
     this.setState({
-      selectRecipe: {"name": recipe_index,
-        "Ingredients":this.state.recipe[recipe_index].Ingredients,
-    "Instructions":this.state.recipe[recipe_index].Instructions}
+      selectRecipe: {}
     });
+    }
+    if(type==="Edit"){
+      this.setState({
+        selectRecipe: {}
+      });
+      this.setState({
+          editRecipe: {
+          "name": recipe_index,
+          "Ingredients":this.state.recipe[recipe_index].Ingredients,
+          "Instructions":this.state.recipe[recipe_index].Instructions}
+        });  
+    }
+    if(type==="View"){
+      this.setState({
+        selectRecipe: {
+          "name": recipe_index,
+          "Ingredients":this.state.recipe[recipe_index].Ingredients,
+          "Instructions":this.state.recipe[recipe_index].Instructions}
+      });
+      this.setState({
+        editRecipe: {}
+      });
+    }
+    else{
+      console.log("No type")
+    }
+    console.log(this.state);
   },
-  render : function() {
+  editChange:function(name,ing,inst){
+      this.setState({
+          editRecipe: {
+            "name": name,
+            "Ingredients":ing,
+            "Instructions":inst}
+        });
+  },
+  renderType(){
+    if(this.state.type==="Edit"){
+      return (<EditRecipePane editRecipe={this.state.editRecipe} editChange={this.editChange}/>);
+    } 
+    if(this.state.type==="View"){
+      return (<RecipeDetailPane selectRecipe={this.state.selectRecipe}/>);
+   }
+   else{
+     console.log("TBC")
+   }
+  },
+  renderBorder(){
+    if(this.state.type===""){
+      return "NoBorder";
+    }
+    else{
+      return "detailBox";
+    }
+  },
+  render: function() {
     return (
       <div>
         <center><h1>Recipe Box</h1></center>
@@ -34,12 +90,14 @@ var App = React.createClass({
                   <th width="15%"><h2>Recipe</h2></th>
                 </tr>
               </thead> 
-                <RecipeList recipe={this.state.recipe} setSelectRecipe={this.setSelectRecipe}/>
+                <RecipeList recipe={this.state.recipe} setSelectRecipe={this.setSelectRecipe} />
             </table>          
             
           </div>
-          <div className="col-md-6 col-xs-6">          
-            <RecipeDetailPane selectRecipe={this.state.selectRecipe}/>
+          <div className="col-md-6 col-xs-6" id={this.renderBorder()}>
+            <table width="100%">
+            {this.renderType()}
+            </table>
           </div>
         </div>
       </div>
@@ -48,7 +106,7 @@ var App = React.createClass({
 });
 
 var RecipeList = React.createClass({
-  renderTableItem: function(name){//<stuck here, need to find how to get the key correctly
+  renderTableItem: function(name){
     return <RecipeItem key={name} name={name} itemRecipe={this.props.recipe[name]} setSelectRecipe={this.props.setSelectRecipe}/>
   },
   render: function() {
@@ -59,15 +117,18 @@ var RecipeList = React.createClass({
 });
 
 var RecipeItem = React.createClass({
-  setSelected: function(){
-    this.props.setSelectRecipe(this.props.name);
+  viewRecipe: function(){
+    this.props.setSelectRecipe(this.props.name, "View");
+  },
+  editRecipe:function(){
+    this.props.setSelectRecipe(this.props.name, "Edit");
   },
   render: function(){
     return (
           <tr key={this.props.name}>
             <td><h4>{this.props.name}</h4>
-              <button type="button" className="btn btn-info" onClick={this.setSelected}>Recipe</button>{" "}
-              <button type="button" className="btn btn-warning">Edit</button>{" "}
+              <button type="button" className="btn btn-info" onClick={this.viewRecipe}>Recipe</button>{" "}
+              <button type="button" className="btn btn-warning" onClick={this.editRecipe}>Edit</button>{" "}
               <button type="button" className="btn btn-danger">Delete</button></td>
           </tr>
     );
@@ -76,13 +137,9 @@ var RecipeItem = React.createClass({
 });
 
 var RecipeDetailPane = React.createClass({
-  displayDetails: function(){
-
-  },
   render: function(){
     return (
-      <table width="100%">
-        <thead>
+      <thead>
           <tr>
             <th width="15%"><h2>{this.props.selectRecipe.name}{console.log(this.props)}</h2></th>
           </tr>
@@ -96,8 +153,47 @@ var RecipeDetailPane = React.createClass({
               <p>{this.props.selectRecipe.Instructions}</p>
             </td>
           </tr>
-        </thead> 
-      </table>
+        </thead>
+    );
+  },
+});
+
+var EditRecipePane = React.createClass({
+  IngredientsChange: function(event){
+      this.props.editChange(this.props.editRecipe.name, event.target.value,this.props.editRecipe.Instructions);
+  },
+  InstructionsChange: function(event){
+      this.props.editChange(this.props.editRecipe.name, this.props.editRecipe.Ingredients, event.target.value);
+  },
+  SaveEdit:function(){
+    console.log(this.props.editRecipe);
+  },
+  CancelEdit:function(){
+    console.log(this.props.editRecipe);
+  },
+  render: function(){
+    return (
+      <thead>
+          <tr>
+            <th width="15%"><h2>{this.props.editRecipe.name}{console.log(this.props)}</h2></th>
+          </tr>
+          <tr key={this.props.editRecipe}>
+            <td><h3>{Object.keys(this.props.editRecipe)[1]}</h3>
+              <textarea rows="3" name="test" id="textIngredients" onChange={this.IngredientsChange} value={this.props.editRecipe.Ingredients}></textarea>
+            </td>
+            </tr>
+            <tr>
+            <td><h3>{Object.keys(this.props.editRecipe)[2]}</h3>
+              <textarea rows="5" id="textInstruction" onChange={this.InstructionsChange} value={this.props.editRecipe.Instructions}></textarea>
+            </td>
+          </tr>
+          <tr>
+            <td>
+            <button type="button" className="btn btn-danger pull-right" id="editButton">Cancel</button>{" "}
+            <button type="button" onClick={this.SaveEdit} className="btn btn-success pull-right" id="editButton">Save</button>
+            </td>
+          </tr>
+      </thead>
     );
   },
 });
