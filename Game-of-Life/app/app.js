2fbcbb1530cab0f0325035f3d6f3c72a2f5f6578
Silver Dn <Antonion90@gmail.com>
2017-07-04 15:30:57 -0400

    add resetboard function

--- Game-of-Life/app/app.js
+++ Game-of-Life/app/app.js
@@ -6,13 +6,22 @@ var blankboard = new Array(10);
 for (var i=0;i<blankboard.length;i++){
   blankboard[i]=new Array(5);
 }
+function resetboard(board){
+  for(var x=0;x<board.length;x++){
+    for(var y=0;y<board.length;y++){
+      board[x][y]="dead";
+    }
+  }
+}
+
+resetboard(blankboard);
 
 var App = React.createClass({
 
   render: function() {
     return (
       <div>
-        <center><h1>Recipe Box</h1></center>
+        <center><h1>Play Game of Life</h1></center>
         <div id="header"></div>
         <div className="container">
           <div className="col-md-6 col-xs-6">
