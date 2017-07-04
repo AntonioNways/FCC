69a16386c68c30add938a2a4efcd44a28234000d
Silver Dn <Antonion90@gmail.com>
2017-07-04 02:45:39 -0400

    Create blankboard for game of life

--- Game-of-Life/app/app.js
+++ Game-of-Life/app/app.js
@@ -2,7 +2,10 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Modal= require('react-bootstrap').Modal;
 
-
+var blankboard = new Array(10);
+for (var i=0;i<blankboard.length;i++){
+  blankboard[i]=new Array(5);
+}
 
 var App = React.createClass({
 
@@ -13,16 +16,9 @@ var App = React.createClass({
         <div id="header"></div>
         <div className="container">
           <div className="col-md-6 col-xs-6">
-            <table width="100%">
-              <thead>
-                <tr>
-                  <th><h2>Recipe <button type="button" className="btn btn-success" onClick={this.renderAdd}>Add + </button></h2></th>
-                </tr>
-              </thead> 
-                <RecipeList recipe={this.state.recipe} setSelectRecipe={this.setSelectRecipe} />
-            </table>          
+            {console.log(blankboard)}
           </div>
-          <div className="col-md-6 col-xs-6" id={this.renderBorder()}>
+          <div className="col-md-6 col-xs-6">
           </div>
           <div>
           </div>
