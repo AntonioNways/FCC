6404d3f449ee4d6b1d9d0af407fdd8d4387c8bff
Silver Dn <Antonion90@gmail.com>
2017-07-11 16:34:04 -0400

    Add hp pack mechanics

--- Roguelike-Dungeon-Crawler-Game/app/app.js
+++ Roguelike-Dungeon-Crawler-Game/app/app.js
@@ -21,28 +21,18 @@ function resetboard(board){
   }
 }
 
-function randomBoard(board,DGNLevel){
-  for(var i=0;i<gHeight;i++){
-    for(var j=0;j<gwidth;j++){
-      var Rnd = Math.floor(Math.random() * 10);
-      var cellState="alive";
-      if (Rnd>4){
-        cellState="dead";
-      }
-      board[i][j]=cellState;
-    }
-  }
-}
 
 function randomizingBox(board){
+  var result;
   var rndHeight = Math.floor(Math.random() *(gHeight-1));
   var rndWidth = Math.floor(Math.random() *(gwidth-1));
   if(board[rndHeight][rndWidth]=="open"){
-    return [rndHeight,rndWidth];
+    result=[rndHeight,rndWidth]
+    if(result!=undefined){
+      return result;
+    }
   }
-  else{
-    randomizingBox(board);
-  }
+  randomizingBox(board);
 }
 
 resetboard(blankboard);
@@ -56,10 +46,11 @@ var App = React.createClass({
       "dungeon": 1,
       "enemyNum":5,
       "enemyLoc": {},
+      "playerHP": 100,
       "hpPack": 5, 
       "hpLocation":{},
-      "weopon": "Fist", 
-      "WepLoc":{},
+      "weapon": "Fist", 
+      "WeapLoc":{},
     };
   },
   renderStage:function(){
@@ -67,7 +58,8 @@ var App = React.createClass({
 
     //HP pack
     var OldhpLocation=[];
-    for (var i=0;i<this.state.hpPack;i++){
+    var HPNum=Math.round(this.state.hpPack+Math.random()*(this.state.dungeon));
+    for (var i=0;i<HPNum;i++){
       var HPLoc = randomizingBox(OldBoard);//where HP pack will spawn
       OldBoard[HPLoc[0]][HPLoc[1]]="HP";
       OldhpLocation[i]=HPLoc;
@@ -75,14 +67,16 @@ var App = React.createClass({
 
     //Enemy
     var ELocation=[];
-    for (var j=0;j<this.state.enemyNum;j++){
+    var ENum=Math.round(this.state.enemyNum+Math.random()*(this.state.dungeon*2));
+    for (var j=0;j<ENum;j++){
       var ELoc = randomizingBox(OldBoard);//where HP pack will spawn
+      console.log(ELoc);
       OldBoard[ELoc[0]][ELoc[1]]="Enemy";
       ELocation[j]=ELoc;
     }
     //Weapon
-    var wepLocation = randomizingBox(OldBoard);
-    OldBoard[wepLocation[0]][wepLocation[1]]="Weopen";
+    var weapLocation = randomizingBox(OldBoard);
+    OldBoard[weapLocation[0]][weapLocation[1]]="weapon";
 
     //Player
     var startloc = randomizingBox(OldBoard); //determine where player start
@@ -90,7 +84,7 @@ var App = React.createClass({
     this.setState({
       "playerlocation": startloc,
       "enemyLoc": ELocation,
-      "WepLoc": wepLocation,
+      "WeapLoc": weapLocation,
       "hpLocation": OldhpLocation
     })
   },
@@ -98,9 +92,25 @@ var App = React.createClass({
     this.renderStage();
   },
   PlayerMovement(newY,newX,){
+    var OldBoard = Object.assign([],this.state.board);
+    var HPArray = Object.assign([],this.state.hpLocation);
       if(newY<gHeight&&newY>-1&&newX>-1&&newX<gwidth){
         var nextloc = this.state.board[newY][newX];
-        if(nextloc=="open"){
+        if(nextloc=="open"||nextloc=="HP"){
+            //game mechanic for HP pack
+            if(nextloc=="HP"){
+              var NewHP=[];
+              OldBoard[newY][newX]="open";
+              for(var h=0;h<HPArray.length;h++){
+                if(HPArray[h][0]!==newY&&HPArray[h][1]!==newX){
+                  NewHP.push([HPArray[h][0],HPArray[h][1]]);       
+                }
+              }
+              this.setState({
+                "playerHP": this.state.playerHP+20,
+                "hpLocation": NewHP,
+              });
+            }
             this.setState({
               "playerlocation": [newY,newX]
             });
@@ -137,7 +147,7 @@ var App = React.createClass({
           </div>
           <div className="col-md-12 col-xs-12">
             
-              <BoardPane board={this.state.board} playerlocation={this.state.playerlocation} hpLocation={this.state.hpLocation} enemyLoc={this.state.enemyLoc} WepLoc={this.state.WepLoc}/>
+              <BoardPane board={this.state.board} playerlocation={this.state.playerlocation} hpLocation={this.state.hpLocation} enemyLoc={this.state.enemyLoc} WeapLoc={this.state.WeapLoc}/>
             
           </div>
         </div>
@@ -161,9 +171,9 @@ var BoardPane = React.createClass({
           <rect key={"y"+y+"x"+x} x={x} y={y} width="13" height="13" fill="green" stroke="green"/>
     );
   },
-  renderWeopen: function(a,b){ 
-    var x=Number(this.props.WepLoc[1]*13);
-    var y=Number(this.props.WepLoc[0]*13);
+  renderWeapen: function(a,b){ 
+    var x=Number(this.props.WeapLoc[1]*13);
+    var y=Number(this.props.WeapLoc[0]*13);
     return (
           <rect key={"y"+a+"x"+b} x={x} y={y} width="13" height="13" fill="yellow" stroke="yellow"/>
     );
@@ -182,7 +192,7 @@ var BoardPane = React.createClass({
             <rect x="0" y="0" width="780" height="325" fill="white"/>
             {this.props.hpLocation.map(this.renderHP)}
             {this.props.enemyLoc.map(this.renderEnemy)}
-            {this.props.WepLoc.map(this.renderWeopen)}
+            {this.props.WeapLoc.map(this.renderWeapen)}
             {this.props.playerlocation.map(this.renderPlayer)}
           </svg>
         </div>
